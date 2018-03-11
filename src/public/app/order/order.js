f968276176556adcfa4259f5a023a21a9211b5f9
Tim Van Cleave <codingfiddler@gmail.com>
2018-03-10 22:15:22 -0600

    Fixed odd column sizing issues with order page cards, created submit method stub for form validation.

--- src/public/app/order/order.js
+++ src/public/app/order/order.js
@@ -9,6 +9,14 @@ angular.module('myApp.order', ['ngRoute'])
   });
 }])
 
-.controller('OrderCtrl', [function() {
+.controller('OrderCtrl', ['$scope', function($scope) {
+   $scope.init = function(){
+     $scope.states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+   };
 
+   $scope.submit = function() {
+      console.log($scope.shippingForm);
+   };
+
+   $scope.init();
 }]);
